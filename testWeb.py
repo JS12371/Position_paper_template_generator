e05676549108f281a11b4cbf03b9f9c367b23259
Jonah Smith <125199400+JS12371@users.noreply.github.com>
2024-05-13 13:25:17 -0400

    Update testWeb.py

--- testWeb.py
+++ testWeb.py
@@ -116,7 +116,7 @@ def get_issue_content(issue):
 
  
 
-   filename = f"IssuestoArgs/{issue}.txt"  
+   filename = f"IssuestoArgs/DRS.txt"  
    try:  
        with open(filename, 'r') as file:  
            content = file.read()  
