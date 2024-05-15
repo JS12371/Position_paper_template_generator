312cf899c8c29eb7f141ed4a22696daab91d1138
Jonah Smith <125199400+JS12371@users.noreply.github.com>
2024-05-15 14:53:19 -0400

    Update testWeb.py

--- testWeb.py
+++ testWeb.py
@@ -623,27 +623,6 @@ def create_word_document(case_data):
 
  
 
-    i = 1 
- 
-    if issue == 'Issue not found':
-       pass
-    else: 
-       while i < len(issue): 
-
-          issue_content = get_issue_content(issue[i]) 
-
-          header = doc.add_paragraph(f"Issue {i+1}: {issue_content} \n\n") 
-
-          run = header.add_run() 
-
-          run.font.size = Pt(11) 
-
-          run.font.name = 'Arial' 
-
-          i += 1 
-
- 
-
  
 
  
