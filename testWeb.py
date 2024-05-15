4d8d624bc0839cada66e7890fc77a7fd744f3545
Jonah Smith <125199400+JS12371@users.noreply.github.com>
2024-05-15 10:46:43 -0400

    Update testWeb.py

--- testWeb.py
+++ testWeb.py
@@ -174,7 +174,7 @@ def create_word_document(case_data):
 
     case_name = case_data['Case Name'].iloc[0] if 'Case Name' in case_data else 'Case Name not found' 
 
-    issue = case_data['Issue'].unique() if 'Issue' in case_data else 'Issue not found' 
+    issue = case_data['Issue'].unique() if 'Issue' in case_data or case_data['Issue Typ'] if 'Issue Typ' in case_data else 'Issue not found' 
 
     provider_numbers = ', '.join(case_data['Provider ID'].unique()) if 'Provider ID' in case_data else 'Provider Numbers not found' 
 
