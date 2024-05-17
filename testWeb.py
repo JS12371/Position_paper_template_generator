a97d98c7a50106c39354f14beab676d9c596c584
Jonah Smith <125199400+JS12371@users.noreply.github.com>
2024-05-17 11:38:51 -0400

    Update testWeb.py

--- testWeb.py
+++ testWeb.py
@@ -693,11 +693,10 @@ create_doc = st.button('Create Document')
 if uploaded_file and case_num and create_doc:  
     try:  
         df = pd.read_excel(uploaded_file) 
-        try:
-            docx_file = create_word_document(find_case_data(df, case_num))
-            st.markdown(get_download_link(docx_file, f'Case_{case_num}.docx'), unsafe_allow_html=True)
-        except:
-            st.write('Case not found in the spreadsheet. Please try again with a different case number.')
+        
+        docx_file = create_word_document(find_case_data(df, case_num))
+        st.markdown(get_download_link(docx_file, f'Case_{case_num}.docx'), unsafe_allow_html=True)
+        st.write('Case not found in the spreadsheet. Please try again with a different case number.')
 
     except: 
 
