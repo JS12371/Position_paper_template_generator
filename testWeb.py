8a8c1b43aca3158f178fe30415ab56e8eda4e13d
Jonah Smith <125199400+JS12371@users.noreply.github.com>
2024-05-21 10:15:02 -0400

    Update testWeb.py

--- testWeb.py
+++ testWeb.py
@@ -93,10 +93,10 @@ def format_date(date):
 def get_issue_content(issue):  
     # Function to read content from {issue}.txt file  
     issueformatted = issue.replace(" ", "") 
-    filename = f"IssuestoArgs/{issueformatted}.txt"  
+    filename = f"IssuestoArgs/{issueformatted}.docx"  
     try: 
-        with open(filename, 'r') as file:  
-            content = file.read()  
+        document = Document(filename)
+        content = "\n".join([para.text for para in document.paragraphs])
         return content  
     except FileNotFoundError:  
         ## if issue starts with 'Transfer', then it is a transferred issue
