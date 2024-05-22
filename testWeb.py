4eb6af95cec20f0e6f04f980d4737069fae5ed53
Jonah Smith <125199400+JS12371@users.noreply.github.com>
2024-05-22 11:36:51 -0400

    Update testWeb.py

--- testWeb.py
+++ testWeb.py
@@ -613,7 +613,7 @@ def create_word_document(case_data):
         if len(adj_no) > 1:
             adj_no = "Various"
         header = doc.add_paragraph(f"\n\nIssue(s): {issues}\n\nAdjustment No(s): {adj_no}\n\nApproximate Reimbursement Amount: N/A")
-        run = header_add_run()
+        run = header.add_run()
         run.font.size = Pt(11)
         run.font.name = 'Times New Roman'
 
