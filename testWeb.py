33b3748021295f9ea797b583a998901f5b4592b5
Jonah Smith <125199400+JS12371@users.noreply.github.com>
2024-05-24 11:25:28 -0400

    Update testWeb.py

--- testWeb.py
+++ testWeb.py
@@ -135,7 +135,7 @@ def copy_runs(src_paragraph, dest_paragraph):
 # Function to copy paragraphs from source to destination
 def copy_paragraphs(src, dest):
     for paragraph in src.paragraphs:
-        if paragraph.text.strip() != '':
+        if paragraph.text.strip() != '' or paragraph.text.strip() != '\0':
             dest_paragraph = dest.add_paragraph()
             copy_paragraph_format(paragraph, dest_paragraph)
             copy_runs(paragraph, dest_paragraph)
