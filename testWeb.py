a919767c7652fe44b8549ca9c302bad234559076
Jonah Smith <125199400+JS12371@users.noreply.github.com>
2024-06-20 10:53:45 -0400

    Update testWeb.py

--- testWeb.py
+++ testWeb.py
@@ -511,8 +511,8 @@ def get_download_link(file, filename):
 # New function to check if an issue is transferred
 def is_transferred_issue(issue, case_data):
     transferred_to_case = case_data['Transferred to Case #'] if 'Transferred to Case #' in case_data else ['transferred to case not found']
-    for i in range(len(transferred_to_case) - 1):
-        st.write(f"{transferred_to_case[i]}")
+    for i in transferred_to_case:
+        st.write(f"{i}")
     temptransferred_to_case = [i for i in transferred_to_case]
     transferred_to_case = temptransferred_to_case
 
