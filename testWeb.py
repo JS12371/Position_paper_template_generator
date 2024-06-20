23d308ffdd6e5617b49ead43d44c0ac67ba9646a
Jonah Smith <125199400+JS12371@users.noreply.github.com>
2024-06-20 10:28:08 -0400

    Update testWeb.py

--- testWeb.py
+++ testWeb.py
@@ -588,7 +588,9 @@ if st.session_state.df is not None:
         if not st.session_state.case_data.empty:
             issues = st.session_state.case_data['Issue'].unique()
             # Filter out transferred issues
+            st.write(f"{issues}")
             issues = [issue for issue in issues if not is_transferred_issue([issue], st.session_state.case_data)]
+            st.write(f"{issues}")
             for issue in issues:
                 arguments = get_possible_arguments(issue)
                 if arguments:
