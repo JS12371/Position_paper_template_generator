b7c1667445a0d2e45c31e96c984bcd18b52000a8
Jonah Smith <125199400+JS12371@users.noreply.github.com>
2024-07-01 11:17:56 -0400

    Update testWeb.py

--- testWeb.py
+++ testWeb.py
@@ -65,7 +65,7 @@ def get_issue_content(issue, selected_argument):
 def get_possible_arguments(issue):
     issueformatted = sanitize_filename(issue)
     files = glob.glob(f"IssuestoArgs/{issueformatted}_*.docx")
-    arguments = [os.path.basename(f).replace(f"{issueformatted}_", "").replace(".docx", "") for f in files]
+    arguments = [os.path.basename(f).replace(f"{issueformatted}_", f"{issueformatted}_").replace(".docx", "") for f in files]
     return arguments
 
 
