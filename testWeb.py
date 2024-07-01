45c098de6484fce4f877658b7cc562feff88b077
Jonah Smith <125199400+JS12371@users.noreply.github.com>
2024-07-01 13:10:23 -0400

    Update testWeb.py

--- testWeb.py
+++ testWeb.py
@@ -591,7 +591,6 @@ if st.session_state.df is not None:
                     tempiss.append("Transferred Case")
 
             issues = tempiss
-            issues = list(set(issues))
             for issue in issues:
                 arguments = get_possible_arguments(issue)
                 if arguments:
