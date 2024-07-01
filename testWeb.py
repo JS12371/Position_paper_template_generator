533e4ced293f38fb7b37b0e777efd2e05bb3de8c
Jonah Smith <125199400+JS12371@users.noreply.github.com>
2024-07-01 13:40:05 -0400

    Update testWeb.py

--- testWeb.py
+++ testWeb.py
@@ -178,18 +178,9 @@ def create_word_document(case_data, selected_arguments):
     case_num = case_data['Case Num'].iloc[0] if 'Case Num' in case_data else 'Case Num not found'
     case_name = case_data['Case Name'].iloc[0] if 'Case Name' in case_data else '<input provider name>'
     issue = case_data['Issue'] if 'Issue' in case_data else ['Issue not found']
+    cloneissue = [iss for iss in issue]
 
-    try:
-        for i in range(len(issue)):
-            if issue[i] == 'Not in the spreadsheet':
-                issue.pop(i)
-    except:
-        pass
-        
     tempissue = [i for i in issue]
-
-    cloneissue = [iss for iss in tempissue]
-    
     issue = tempissue
 
     transferred_to_case = case_data['Transferred to Case #'] if 'Transferred to Case #' in case_data else ['transferred to case not found']
@@ -239,7 +230,7 @@ def create_word_document(case_data, selected_arguments):
     else:
         pass
 
-    if issue[0].startswith('Transfer') and group_mode:
+    if issue[0].startswith('Transfer'):
         issue.remove(issue[0])
     date_of_appeal = format_date(str(case_data['Appeal Date'].iloc[0])[:10]) if 'Appeal Date' in case_data else 'Date of Appeal not found'
     adj_no = ','.join(case_data['Audit Adj No.'].unique()) if 'Audit Adj No.' in case_data else 'Audit Adj No. not found'
@@ -369,10 +360,9 @@ def create_word_document(case_data, selected_arguments):
     run = header.add_run()
     run.font.color.rgb = RGBColor(0, 0, 0)
 
+    st.write(f"{issue}")
+
     for i in range(len(issue)):
-        if issue[i] == 'Not in the spreadsheet':
-            issue.pop(i)
-            continue
         if group_mode:
             if len(adj_no) > 1:
                 adj_no = "Various"
@@ -400,77 +390,77 @@ def create_word_document(case_data, selected_arguments):
     i = 0
     issue = list(filter(None, issue))
 
-    all_law_regulations = {'Law': [], 'Regulations': [], 'Program Instructions': [], 'Other Sources': [], 
-                           'United States Statutes': [], 'Judicial Decisions': [], 'Agency Decisions': [], 'SSA': [],
-                           'Federal Register': [], 'Agency Instructions': [], 'Case Law': []}
-    all_exhibits = Document()
-    
-    for i, issue in enumerate(issue):
-        # Skip issues that are marked as "Transferred"
-        st.write(selected_arguments)
-        if issue.startswith("Transferred"):
-            continue
-        
-        header = doc.add_paragraph(f"\nIssue {i+1}: {issue}")
-        run = header.add_run()
-        run.font.color.rgb = RGBColor(0, 0, 0)
-        issue_doc, error = get_issue_content(issue, selected_arguments[i])
-        if error:
-            header = doc.add_paragraph(f"{error}\n")
-        else:
-            exhibits = extract_exhibits(issue_doc)
-            remove_exhibits_from_document(issue_doc)
-            law_regulations = extract_law_regulations(issue_doc)
-            remove_law_regulations_from_document(issue_doc)
+    st.write(f"{issue}")
+
+    if issue[0] == 'Issue not found':
+        pass
+    else:
+        all_law_regulations = Document()
+        all_exhibits = Document()
+        while i < len(issue):
+            # Skip issues that are marked as "Transferred"
+            if issue[i].startswith("Transferred"):
+                i += 1
+                continue
+            
+            header = doc.add_paragraph(f"\nIssue {i+1}: {issue[i]}")
+            run = header.add_run()
+            run.font.color.rgb = RGBColor(0, 0, 0)
+            issue_doc, error = get_issue_content(issue[i], selected_arguments[i])
+            if error:
+                header = doc.add_paragraph(f"{error}\n")
+            else:
+                exhibits = extract_exhibits(issue_doc)
+                remove_exhibits_from_document(issue_doc)
+                law_regulations = extract_law_regulations(issue_doc)
+                remove_law_regulations_from_document(issue_doc)
+                composer = Composer(doc)
+                composer.append(issue_doc)
+                for section, paragraphs in law_regulations.items():
+                    if paragraphs:
+                        header = all_law_regulations.add_paragraph(f"\n{section}:")
+                        header.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
+                        run = header.runs[0]
+                        run.font.bold = True
+                        run.font.color.rgb = RGBColor(0, 0, 0)
+                        for paragraph in paragraphs:
+                            all_law_regulations.add_paragraph(paragraph)
+                if exhibits:
+                    header = all_exhibits.add_paragraph(f"\nISSUE: {issue[i]}")
+                    header.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
+                    run = header.runs[0]
+                    run.font.bold = True
+                    run.font.color.rgb = RGBColor(0, 0, 0)
+                    for exhibit in exhibits:
+                        all_exhibits.add_paragraph(exhibit.text)
+            run = header.add_run()
+            run.font.color.rgb = RGBColor(0, 0, 0)
+            i += 1
+
+        # Append all law and regulations to the main document
+        if all_law_regulations.paragraphs:
+            # Ensure there's no extra blank page before this section
+            if doc.paragraphs[-1].text.strip():
+                doc.add_page_break()
+            header = doc.add_paragraph('IV. LAW, REGULATIONS, AND PROGRAM INSTRUCTIONS')
+            header.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
+            run = header.runs[0]
+            run.font.bold = True
+            run.font.color.rgb = RGBColor(0, 0, 0)
+
             composer = Composer(doc)
-            composer.append(issue_doc)
-            
-            # Combine law and regulations
-            for section, paragraphs in law_regulations.items():
-                all_law_regulations[section].extend(paragraphs)
-            
-            if exhibits:
-                header = all_exhibits.add_paragraph(f"\nISSUE: {issue}")
-                header.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
-                run = header.runs[0]
-                run.font.bold = True
-                run.font.color.rgb = RGBColor(0, 0, 0)
-                for exhibit in exhibits:
-                    all_exhibits.add_paragraph(exhibit.text)
-        
-        run = header.add_run()
-        run.font.color.rgb = RGBColor(0, 0, 0)
-    
-    # Append all combined law and regulations to the main document
-    if any(all_law_regulations.values()):
-        if doc.paragraphs[-1].text.strip():
-            doc.add_page_break()
-        header = doc.add_paragraph('IV. LAW, REGULATIONS, AND PROGRAM INSTRUCTIONS')
-        header.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
-        run = header.runs[0]
-        run.font.bold = True
-        run.font.color.rgb = RGBColor(0, 0, 0)
-        
-        for section, paragraphs in all_law_regulations.items():
-            if paragraphs:
-                header = doc.add_paragraph(f"\n{section}:")
-                header.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
-                run = header.runs[0]
-                run.font.bold = True
-                run.font.color.rgb = RGBColor(0, 0, 0)
-                for paragraph in paragraphs:
-                    doc.add_paragraph(paragraph)
+            composer.append(all_law_regulations)
 
-    # Append all exhibits to the main document at the end
-    if all_exhibits.paragraphs:
-        header = doc.add_paragraph('\nV. EXHIBITS')
-        header.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
-        run = header.runs[0]
-        run.font.bold = True
-        run.font.color.rgb = RGBColor(0, 0, 0)
+        # Append all exhibits to the main document at the end
+        if all_exhibits.paragraphs:
+            header = doc.add_paragraph('\nV. EXHIBITS')
+            header.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
+            run = header.runs[0]
+            run.font.bold = True
+            run.font.color.rgb = RGBColor(0, 0, 0)
 
-        composer = Composer(doc)
-        composer.append(all_exhibits)
+            composer = Composer(doc)
+            composer.append(all_exhibits)
 
     for paragraph in doc.paragraphs:
         for run in paragraph.runs:
@@ -490,7 +480,6 @@ def create_word_document(case_data, selected_arguments):
     buffer = BytesIO()
     doc.save(buffer)
     return buffer.getvalue()
-
   
 def string_processing(s): 
     if pd.isnull(s) or s == '': 
@@ -566,35 +555,7 @@ if st.session_state.df is not None:
     # Proceed only if the case_data is found
     if st.session_state.case_data is not None:
         if not st.session_state.case_data.empty:
-            issues = st.session_state.case_data['Issue']
-            transferred_to_case = st.session_state.case_data['Transferred to Case #']
-
-            temptrans = []
-            
-            for i in transferred_to_case:
-                temptrans.append(i)
-
-            transferred_to_case = temptrans
-
-            tempiss = []
-
-            for i in issues:
-                tempiss.append(i)
-
-            issues = tempiss
-
-            tempiss = []
-            
-            for i in range(len(issues)):
-                if transferred_to_case[i] == 'Not in the spreadsheet':
-                    tempiss.append(issues[i])
-                else:
-                    tempiss.append(f"Transferred Case #{i}")
-
-            issues = tempiss
-
-            issues = set(issues)
-            
+            issues = st.session_state.case_data['Issue'].unique()
             for issue in issues:
                 arguments = get_possible_arguments(issue)
                 if arguments:
@@ -603,15 +564,6 @@ if st.session_state.df is not None:
                     st.session_state.selected_arguments[issue] = selected_argument
                 else:
                     st.session_state.selected_arguments[issue] = ""
-
-
-            issues = list(issues)
-            
-
-            # Use a list comprehension to filter out issues that start with "Transf"
-            issues = [issue for issue in issues if not issue.startswith("Transf")]
-
-            st.write(issues)
 
             # Step 3: Create Document
             create_doc = st.button('Create Document') 
